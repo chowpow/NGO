52902e67e5faef7180ec981cdf27a60514161202
Shaochen <shaochenma12@gmail.com>
2020-11-08 14:16:50 -0800

    Tried to fix database settings

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Hello");
-        System.out.println("virgin");
-        System.out.println("U sexy fuck");
-        System.out.println("cum ci cum sa");
     }
 }

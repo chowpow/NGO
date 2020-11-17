0e0c28e0dbfda1ff9f3ebe5ea4c3269cf9b58550
danielkim <danielkim879@gmail.com>
2020-11-16 20:52:15 -0800

    hello bossman again

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Hello");
-        System.out.println("byeee");
+        System.out.println("Hello");
+
     }
 }

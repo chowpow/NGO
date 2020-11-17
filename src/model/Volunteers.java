c59ea8096d2b4b875a39cf7d190eee485dda2863
danielkim <danielkim879@gmail.com>
2020-11-16 22:20:31 -0800

    finished volunteers

---
+++ src/model/Volunteers.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Volunteers {
+    private final int volunteerID;
+    private final String password;
+    private final String name;
+    private final int phoneNumber;
+    private final String address;
+    private final String city;
+
+
+
+    public Volunteers(int volunteerID, String password, String name, int phoneNumber, String address, String city) {
+        this.volunteerID = volunteerID;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.city = city;
+    }
+
+    public int getVolunteerID() {
+        return volunteerID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public String getCity() {
+        return city;
+    }
+}
